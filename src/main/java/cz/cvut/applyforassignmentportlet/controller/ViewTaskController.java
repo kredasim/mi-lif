5f20908f8ef160742c1c05d7996bb52fae76a904
Simeon Kredatus <kredasim@fit.cvut.cz>
2012-12-16 18:51:46 +0100

    Creating dummy service layer.

--- src/main/java/cz/cvut/applyforassignmentportlet/controller/ViewTaskController.java
+++ src/main/java/cz/cvut/applyforassignmentportlet/controller/ViewTaskController.java
@@ -5,35 +5,25 @@
 package cz.cvut.applyforassignmentportlet.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.portlet.PortletContext;
 
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
-
-import cz.cvut.applyforassignmentportlet.dto.AttachmentDto;
-import cz.cvut.applyforassignmentportlet.dto.ProcessRoleDto;
-import cz.cvut.applyforassignmentportlet.dto.ProjectDto;
-import cz.cvut.applyforassignmentportlet.dto.SkillDto;
+import cz.cvut.fit.bpm.api.dto.AttachmentDto;
+import cz.cvut.fit.bpm.api.dto.SkillDto;
+import cz.cvut.fit.bpm.api.dto.TaskDto;
+import cz.cvut.fit.bpm.api.dto.UnitRoleDto;
+import cz.cvut.fit.bpm.api.service.SkillDtoService;
+import cz.cvut.fit.bpm.api.service.TaskDtoService;
+import cz.cvut.fit.bpm.common.PortletCommonUtils;
+import cz.cvut.fit.bpm.common.WorkflowService;
 import cz.cvut.fit.industry.api.WorkflowConstants;
-import cz.cvut.fit.industry.api.dto.RoleDto;
-import cz.cvut.fit.workflow.common.WorkflowService;
 
 /**
- *
- * @author simo
- */
-/**
+ * Controller for view page of this portlet.
  * @author Simeon Kredatus
  *
  */
@@ -45,105 +35,29 @@ public class ViewTaskController implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 9037053633902916249L;
-	
-	private ProjectDto project;
-    private List<ProcessRoleDto> roles;
-    private List<SkillDto> mySkills;
-    private List<AttachmentDto> attachments;
     
     private static String GREEN_COLOR = "green";
     private static String RED_COLOR = "red";
 
     @ManagedProperty("#{workflowService}")
     private WorkflowService workflowService;
+    
+    @ManagedProperty("#{dummySkillDtoService}")
+    private SkillDtoService skillDtoService;
+    
+    @ManagedProperty("#{dummyTaskDtoService}")
+    private TaskDtoService taskDtoService;
+
+
     /**
-     * Creates a new instance of ViewTaskController
-     */
-    public ViewTaskController() {
-    }
-
-    @PostConstruct
-    public void init() {
-    	project = new ProjectDto();
-        project.setName("project1");
-        project.setTargets("Some targets");
-        project.setDescription("descriptioooon");
-        project.setDesiredOutputs("desiredOutputssadfkjzm,nzc,");
-        project.setLogonDeadline(new Date());
-        project.setNominatedSubject("PA1");
-        project.setProject("Parent project -> child project");
-        project.setValidFrom(new Date());
-        project.setValidTo(new Date());
-        project.setWorkloadEstimation(100L);
-        project.setSources("some sources");
-
-        initRoles();
-        initAttachments();
-    }
-
-    //TODO finish after the method of parameter parsing will be known.
-    /**
+     * TODO finish after the method of parameter passing will be known.
      * Method starts new process instance of process called {@link WorkflowConstants.APPLY_FOR_TASK_PROCESS_KEY}.
      * Then the usertask 1 is executed to apply for the offered role.
      * @param roleId
      */
     public void applyFor(int roleId) {
-    	String processId = workflowService.createTask(WorkflowConstants.APPLY_FOR_TASK_PROCESS_KEY);
-    	RoleDto role = convertToRoleDto(roles.get(roleId));
-    	workflowService.completeTaskByProcessId(processId, null);
-    }
-    /** 
-	 * @param processRoleDto
-	 * @return
-	 */
-	private RoleDto convertToRoleDto(ProcessRoleDto processRoleDto) {
-		RoleDto dto = new RoleDto();
-		dto.setDescription(processRoleDto.getDescription());
-		dto.setName(processRoleDto.getName());
-		return dto;
-	}
-
-	public void initAttachments() {
-        attachments = new ArrayList<AttachmentDto>();
-
-        InputStream stream = ((PortletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/text.txt");
-        StreamedContent file = new DefaultStreamedContent(stream, "text/plain", "/text.txt");
-        for (int i = 0; i < 5; i++) {
-            AttachmentDto a = new AttachmentDto();
-            a.setComment("COMMENT " + i);
-            a.setFile(file);
-            attachments.add(a);
-
-        }
-    }
-
-    public void initRoles() {
-        List<SkillDto> sampleSkills = new ArrayList<SkillDto>();
-        for (int i = 0; i < 5; i++) {
-            SkillDto skill = new SkillDto();
-            skill.setName("Skill name " + i);
-            skill.setLevel(i);
-            sampleSkills.add(skill);
-        }
-        mySkills = sampleSkills;
-        
-        roles = new ArrayList<ProcessRoleDto>();
-        for (int i = 0; i < 10; i++) {
-            List<SkillDto> dest = new ArrayList<SkillDto>();
-            for (SkillDto skill : mySkills) {
-                SkillDto s = new SkillDto();
-                s.setLevel(skill.getLevel());
-                s.setName(skill.getName());
-                dest.add(s);
-            }
-            ProcessRoleDto role = new ProcessRoleDto();
-            role.setId("Role" + i);
-            role.setName("Role name " + i);
-            role.setNecessarySkills(dest);
-            role.setDescription("askldfjklasdfjlkasdjflkjvz\nasdklfjaskldfjklasdfjklasdjfzvmzvnm,nvkasldfjlkasdjf");
-            roles.add(role);
-        }
-        mySkills.get(3).setLevel(1);
+    	String processId = workflowService.startProcess(WorkflowConstants.APPLY_FOR_TASK_PROCESS_KEY);
+//    	workflowService.completeTaskByProcessId(null, processId, null);
     }
     
     public String decideColor(String roleName, String skillName) {
@@ -151,68 +65,37 @@ public class ViewTaskController implements Serializable {
         int requiredSkillLevel = getRoleSkillLevel(roleName, skillName);
         if (mySkillLevel >= requiredSkillLevel) {
             return GREEN_COLOR;
-        } else {
-            return RED_COLOR;
         }
+        return RED_COLOR;
     }
     
     public int getSkillLevel(String skillName) {
-        for (SkillDto skill : mySkills) {
-            if (skill.getName().equals(skillName)) {
-                return skill.getLevel();
-            }
-        }
-        return 0;
+    	for (SkillDto skill : skillDtoService.getSkillsForUser(null)) {
+    		if (skill.getType().equals(skillName)) {
+    			return skill.getRating();
+    		}
+    	}
+    	return 0;
     }
     
     public int getRoleSkillLevel(String roleName, String skillName) {
-        for (ProcessRoleDto role : roles) {
-            for (SkillDto skill : role.getNecessarySkills()) {
-                if (role.getName().equals(roleName) && skill.getName().equals(skillName)) {
-                    return skill.getLevel();
-                }
-            }
-        }
-        return 0;
+    	UnitRoleDto unitRoleDto = PortletCommonUtils.getUnitRoleByName(getTask().getRoles(), roleName);
+    	for (SkillDto skill : unitRoleDto.getSkillDtoList()) {
+    		if (skill.getType().equals(skillName)) {
+    			return skill.getRating();
+    		}
+    	}
+    	return 0;
     }
 
     public void downloadAttachment(AttachmentDto attachment) throws IOException {
-
         FacesContext.getCurrentInstance().getExternalContext().redirect("DownloadServlet-1.0-SNAPSHOT/download.do");
     }
 
-    public List<SkillDto> getMySkills() {
-        return mySkills;
+    public TaskDto getTask() {
+    	return taskDtoService.getById(0L);
     }
-
-    public void setMySkills(List<SkillDto> mySkills) {
-        this.mySkills = mySkills;
-    }
-
-    public ProjectDto getProject() {
-        return project;
-    }
-
-    public void setProject(ProjectDto project) {
-        this.project = project;
-    }
-
-    public List<ProcessRoleDto> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<ProcessRoleDto> roles) {
-        this.roles = roles;
-    }
-
-    public List<AttachmentDto> getAttachments() {
-        return attachments;
-    }
-
-    public void setAttachments(List<AttachmentDto> attachments) {
-        this.attachments = attachments;
-    }
-
+    
 	public WorkflowService getWorkflowService() {
 		return workflowService;
 	}
@@ -220,5 +103,20 @@ public class ViewTaskController implements Serializable {
 	public void setWorkflowService(WorkflowService workflowService) {
 		this.workflowService = workflowService;
 	}
-    
+
+	public SkillDtoService getSkillDtoService() {
+		return skillDtoService;
+	}
+
+	public void setSkillDtoService(SkillDtoService skillDtoService) {
+		this.skillDtoService = skillDtoService;
+	}
+
+	public TaskDtoService getTaskDtoService() {
+		return taskDtoService;
+	}
+
+	public void setTaskDtoService(TaskDtoService taskDtoService) {
+		this.taskDtoService = taskDtoService;
+	}
 }
